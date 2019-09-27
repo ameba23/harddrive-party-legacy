9f0f72210cf20caee140aee58e67f5599d919dd0
ameba23 <ameba23@systemli.org>
2019-09-27 15:25:05 +0200

    add LICENSE

--- index.js
+++ index.js
@@ -6,17 +6,24 @@ const Controller = require('./controller')
 const app = express()
 const router = express.Router()
 
+app.use(function (req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*')
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept')
+  next()
+})
+
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(bodyParser.json())
 
 metadb.ready(() => {
   metadb.buildIndexes(() => {
     const controller = Controller(metadb)
-    router.get('/files', controller.list)
+    router.get('/files', controller.queryFiles)
+    // router.get('/files/:id', controller.files)
     router.get('/myfiles', controller.myFiles)
 
     app.use('/', router)
-// gcc  swarm (req, res) { this.metadb.swarm(argv.key) }
+    // metadb.swarm(key)
   })
 })
 
