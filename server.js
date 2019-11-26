1e22364cc21ffa77f259bff75087ce5cf08f061c
ameba23 <ameba23@systemli.org>
2019-11-26 22:19:54 +0100

    tidy up

--- server.js
+++ server.js
@@ -2,5 +2,5 @@ const app = require('./')
 const port = process.env.PORT || 3000
 app.listen(port)
 
-console.log(require('./metadb-banner.js'))
+console.log(require('./metadb-banner'))
 console.log(`Listening on http://localhost:${port}`)
