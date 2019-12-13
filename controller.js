0caccc226ab678ca148926371384388373ebd9eb
ameba23 <ameba23@systemli.org>
2019-12-14 00:21:14 +0100

    add controllers

--- controller.js
+++ controller.js
@@ -1,27 +1,35 @@
 const pull = require('pull-stream')
 const express = require('express')
 const router = express.Router()
+// const { body } = require('express-validator')
 
 module.exports = function (metadb) {
   router.get('/files', (req, res) => pullback(metadb.queryFiles(), res))
 
   // router.get('/files/:id', controller.files)
+  router.post('/files/:id', (req, res) => { metadb.publishComment(req.body.comment, Callback(res)) })
 
-  router.get('/myfiles', (req, res) => pullback(metadb.myFiles(), res))
+  router.get('/files/myfiles', (req, res) => pullback(metadb.myFiles(), res))
+  router.get('/files/bypeer', (req, res) => pullback(metadb.filesByPeer(req.body.peer), res))
+  router.post('files/search', function (req, res) {
+    return pullback(metadb.filenameSubstring(req.body.searchterm), res)
+  })
 
   router.get('/peers', (req, res) => pullback(metadb.queryPeers(), res))
 
-  router.post('/search', function (req, res) {
-    return pullback(metadb.filenameSubstring(req.body.substring), res)
-  })
+  router.get('/settings', (req, res) => { metadb.getSettings(Callback(res)) })
+  router.post('/settings', (req, res) => { metadb.publishAbout(req.body.name, Callback(res)) })
+
+  router.post('/request', (req, res) => { metadb.publishRequest(req.body.files, Callback(res)) })
+
+  router.post('/swarm', (req, res) => { metadb.swarm(req.body.swarm, Callback(res)) })
+  router.delete('/swarm', (req, res) => { console.log(true, req.body); metadb.unswarm(req.body.swarm, Callback(res)) })
 
   return router
 }
 
 // query (req, res) { return pullback(metadb.query(req.body.query), res) }
 // byExtension (req, res) { return pullback(metadb.byExtention(req.body.extention), res) }
-// publishAbout (req, res) { metadb.publishAbout(req.body.name, Callback(res)) }
-// publishRequest (req, res) { metadb.publishRequest(req.body.files, Callback(res)) }
 // indexFiles (req, res) { metadb.indexFiles(req.body.directory, Callback(res)) }
 
 function Callback (res) {
--- index.js
+++ index.js
@@ -2,23 +2,24 @@ const express = require('express')
 const bodyParser = require('body-parser')
 const metadb = require('metadb')()
 const Controller = require('./controller')
+const cors = require('cors')
 
 const app = express()
-// const router = express.Router()
 
 app.use(function (req, res, next) {
   res.header('Access-Control-Allow-Origin', '*')
+  res.header('Access-Control-Allow-Credentials', true)
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept')
   next()
 })
 
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(bodyParser.json())
+app.use(cors({ origin: '*' }))
 
 metadb.ready(() => {
   metadb.buildIndexes(() => {
     app.use('/', Controller(metadb))
-    // metadb.swarm(key)
   })
 })
 
