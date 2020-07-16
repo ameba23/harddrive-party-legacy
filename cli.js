615fec0ad6593ca26fe81e773d344347c659d80e
ameba23 <ameba23@systemli.org>
2020-07-16 11:01:00 +0200

    basename when displaying name of executable

--- cli.js
+++ cli.js
@@ -2,7 +2,7 @@
 const argv = require('minimist')(process.argv.slice(2))
 const chalk = require('chalk')
 const axios = require('axios')
-const { sep } = require('path')
+const { sep, basename } = require('path')
 const { inspect } = require('util')
 
 const DEFAULTHOST = process.env.METADB_HOST || 'localhost'
@@ -11,7 +11,7 @@ const REQUEST_TIMEOUT = 1000
 
 function displayHelp () {
   console.log(`
-    Usage:  ${chalk.yellow(process.argv[1])} command <options>
+    Usage:  ${chalk.yellow(basename(process.argv[1]))} command <options>
     Commands:
       start - start metadb
       stop - stop metadb
@@ -36,6 +36,7 @@ function displayHelp () {
 if (!argv._.length) {
   displayHelp()
   argv._ = ['settings']
+  process.exit(0)
 }
 
 if (argv.help || argv._[0] === 'help') {
