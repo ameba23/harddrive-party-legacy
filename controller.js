bf7818924d0ace7c455c500e4ebc2f6a6e21ab3e
ameba23 <ameba23@systemli.org>
2021-01-16 19:26:30 +0100

    add swarms endpoints

--- controller.js
+++ controller.js
@@ -13,10 +13,9 @@ module.exports = function (metadb, options) {
   let uiReady = false
   let buildError
 
-  build(uiFilePath, options, (err) => {
-    uiReady = true
-    buildError = err
-  })
+  build(uiFilePath, options)
+    .then(() => { uiReady = true })
+    .catch((err) => { buildError = err })
 
   // Send front-end on get '/'
   router.get('/', (req, res) => {
@@ -26,80 +25,98 @@ module.exports = function (metadb, options) {
   })
 
   // For development - rebuild front end
-  router.get('/rebuild', (req, res) => {
-    build(uiFilePath, options, (err) => {
-      if (err) return res.status(422).send(`Error when building user interface: ${buildError}`)
-      res.type('html').send('<html><body>Rebuilt... <a href="http://localhost:2323">reload</a></body></html>')
-      // res.sendFile(uiFilePath)
-    })
+  router.get('/rebuild', async (req, res) => {
+    await build(uiFilePath, options)
+      .catch((err) => {
+        return res.status(422).send(`Error when building user interface: ${err.message}`)
+      })
+    res.type('html').send('<html><body>Rebuilt... <a href="http://localhost:2323">reload</a></body></html>')
+    // res.sendFile(uiFilePath)
   })
 
   // List files
-  router.get('/files', (req, res) => pullback(metadb.query.files(req.query), res, req.query.LIMIT))
+  router.get('/files', (req, res) => processIterator(metadb.query.files.stream(), res, req.query.LIMIT)) // pass req.query
   // List files in order they were added
-  router.get('/files/chronological', (req, res) => pullback(metadb.files.pullStreamByTimestamp({ reverse: true }), res))
+  router.get('/files/chronological', (req, res) => processIterator(metadb.query.files.byTimestamp({ reverse: true }), res))
+
   // List own files
-  router.get('/files/shares', (req, res) => pullback(metadb.query.ownFilesNewestFirst(), res))
+  router.get('/files/shares', (req, res) => processIterator(metadb.listSharesNewestFirst(), res))
+
   // List files of a given peer
-  router.get('/files/bypeer/:peerId', (req, res) => pullback(metadb.query.filesByPeer(req.params.peerId), res))
+  router.get('/files/bypeer/:peerId', (req, res) => processIterator(metadb.query.files.byHolder({ holder: req.params.peerId }), res))
+
   // List files matching a given subdirectory
-  router.post('/files/subdir', (req, res) => pullback(metadb.query.subdir(req.body.subdir, req.body.opts), res))
+  router.post('/files/subdir', (req, res) => processIterator(metadb.query.files.byPath(req.body.subdir, req.body.opts), res))
+
   // Filename substring search (fulltext coming soon)
-  router.post('/files/search', (req, res) => pullback(metadb.query.filenameSubstring(req.body.searchterm), res))
+  router.post('/files/search', (req, res) => processIterator(metadb.query.files.filenameSubstring(req.body.searchterm), res))
 
   // Index a local directory
-  router.post('/files/index', (req, res) => { metadb.indexFiles(req.body.dir, req.body.options, Callback(res)) })
-  // Stop indexing a local directory
-  router.delete('/files/index', (req, res) => { metadb.cancelIndexing(req.body.dir, Callback(res)) })
-  // Pause indexing a local directory TODO dont use GET
-  router.get('/files/index/pause', (req, res) => { metadb.pauseIndexing(Callback(res)) })
-  // Resume indexing a local directory TODO dont use GET
-  router.get('/files/index/resume', (req, res) => { metadb.resumeIndexing(Callback(res)) })
-  // Get total number of files shared in each shared directory
-  router.get('/share-totals', (req, res) => { pullback(metadb.getShareTotals(), res) })
-
-  // Get metadata about a file with a given hash
-  router.get('/files/:id', (req, res) => { metadb.files.get(req.params.id, Callback(res)) })
-  // Add a comment to a file with a given hash
-  router.post('/files/:id', (req, res) => { metadb.publish.fileComment(Object.assign(req.body, { sha256: req.params.id }), Callback(res)) })
-
-  // List peers
-  router.get('/peers', (req, res) => metadb.query.peers(Callback(res)))
-
-  // List settings
-  router.get('/settings', (req, res) => { metadb.getSettings(Callback(res)) })
-  // Update settings
-  router.post('/settings', (req, res) => { metadb.setSettings(req.body, Callback(res)) })
-
-  // List requested files
-  router.get('/request', (req, res) => pullback(metadb.query.requesting(), res))
-  // Request files with given hashes
-  router.post('/request', (req, res) => { metadb.request(req.body.files, Callback(res)) })
-  // Cancel requests for files with given hashes
-  router.delete('/request', (req, res) => { metadb.unrequest(req.body.files, Callback(res)) })
-
-  // Connect to a given swarm
-  router.post('/swarm', (req, res) => { metadb.swarm.connect(req.body.swarm, Callback(res)) })
-  // Disconnect from a given swarm
-  router.delete('/swarm', (req, res) => { metadb.swarm.disconnect(req.body.swarm, Callback(res)) })
-
-  // List downloaded files
-  router.get('/downloads', (req, res) => { pullback(metadb.getDownloads(), res) })
-  // List uploaded files
-  router.get('/uploads', (req, res) => { pullback(metadb.getUploads(), res) })
-  // Send a downloaded file to the browser
-  router.get('/downloads/:hash', (req, res) => {
-    metadb.getDownloadedFileByHash(req.params.hash, (err, fileObject) => {
-      if (err) return res.status(422).json({ err: 'No such file' })
-      const filepath = path.join(metadb.config.downloadPath, fileObject.name)
-      res.sendFile(filepath)
+  router.post('/files/index', (req, res) => {
+    metadb.shares.once('start', (status) => {
+      res.status(200).json(status)
     })
+    metadb.shares.scanDir(req.body.dir, req.body.options)
+      .catch((err) => {
+        if (!res.finished) res.status(422).json({ error: err.message })
+      })
   })
 
+  // Stop indexing a local directory
+  router.delete('/files/index', (req, res) => { handleSync(metadb.shares.cancel(req.body.dir), res) })
+
+  // Pause indexing a local directory TODO dont use GET
+  router.get('/files/index/pause', (req, res) => { handleSync(metadb.shares.pause(), res) })
+
+  // Resume indexing a local directory TODO dont use GET
+  router.get('/files/index/resume', (req, res) => { handleSync(metadb.shares.resume(), res) })
+
+  // Get total number of files shared in each shared directory
+  router.get('/share-totals', (req, res) => { processIterator(metadb.shares.getShareTotals(), res) })
+
+  // Get metadata about a file with a given hash
+  router.get('/files/:id', (req, res) => { handleAsync(metadb.query.files.get(req.params.id), res) })
+
+  // Add a comment to a file with a given hash
+  router.post('/files/:id', (req, res) => { handleAsync(metadb.fileComment(Object.assign(req.body, { sha256: req.params.id })), res) })
+
+  // List peers
+  router.get('/peers', (req, res) => processIterator(metadb.listPeers(), res))
+
+  // List settings
+  router.get('/settings', (req, res) => { handleAsync(metadb.getSettings(), res) })
+  // Update settings
+  // router.post('/settings', (req, res) => { metadb.setSettings(req.body, Callback(res)) })
+
+  // List requested files
+  router.get('/request', (req, res) => processIterator(metadb.client.wishlist.createReadStream(), res))
+  // Request files with given hashes
+  router.post('/request', (req, res) => { handleAsync(metadb.request(req.body.files), res) })
+  // Cancel requests for files with given hashes
+  // router.delete('/request', (req, res) => { metadb.unrequest(req.body.files, Callback(res)) })
+
+  // Connect to a given swarm
+  router.post('/swarm', (req, res) => { handleSync(metadb.swarm.join(req.body.swarm), res) })
+  // Disconnect from a given swarm
+  router.delete('/swarm', (req, res) => { handleSync(metadb.swarm.leave(req.body.swarm), res) })
+
+  // List downloaded files
+  router.get('/downloads', (req, res) => { processIterator(metadb.client.getDownloads(), res) })
+  // List uploaded files
+  router.get('/uploads', (req, res) => { processIterator(metadb.server.getUploads(), res) })
+  // Send a downloaded file to the browser
+  // router.get('/downloads/:hash', (req, res) => {
+  //   metadb.getDownloadedFileByHash(req.params.hash, (err, fileObject) => {
+  //     if (err) return res.status(422).json({ err: 'No such file' })
+  //     const filepath = path.join(metadb.config.downloadPath, fileObject.name)
+  //     res.sendFile(filepath)
+  //   })
+  // })
+
   // publish a 'wall message'
-  router.post('/wall-message', (req, res) => { metadb.publish.wallMessage(req.body.message, req.body.swarmKey, Callback(res)) })
+  // router.post('/wall-message', (req, res) => { metadb.publish.wallMessage(req.body.message, req.body.swarmKey, Callback(res)) })
   // get wall messages for a given swarm
-  router.post('/wall-message/by-swarm-key', (req, res) => { pullback(metadb.core.api.wallMessages.pullBySwarmKey(req.body.swarmKey), res) })
+  // router.post('/wall-message/by-swarm-key', (req, res) => { pullback(metadb.core.api.wallMessages.pullBySwarmKey(req.body.swarmKey), res) })
 
   // Open a file locally with xdg-open - currently disabled
   // router.post('/open', (req, res) => {
@@ -112,14 +129,18 @@ module.exports = function (metadb, options) {
   // })
 
   // Stop running
-  router.post('/stop', (req, res) => { metadb.stop(Callback(res)) })
+  router.post('/stop', async (req, res) => {
+    await metadb.stop().catch(handleError(res))
+    res.status(200).json({})
+  })
 
   // Handle websockes
   router.ws('/', (ws, req) => {
-    metadb.events.on('ws', (message) => {
+    metadb.on('ws', (message) => {
+      console.log(122443)
       if (ws.readyState === 1) {
         log('Got message', message, 'sending thru ws')
-        ws.send(message, (err) => {
+        ws.send(JSON.stringify(message), (err) => {
           if (err) log('Error when sending message on ws.', err)
         })
       }
@@ -133,6 +154,22 @@ module.exports = function (metadb, options) {
   return router
 }
 
+function handleSync (output, res) {
+  res.status(200).json(output || {})
+}
+
+function handleAsync (promise, res) {
+  promise
+    .then((output) => { res.status(200).json(output) })
+    .catch((err) => { res.status(422).json({ error: err.message }) })
+}
+
+function handleError (res) {
+  return function (err) {
+    if (!res.finished) res.status(422).json({ error: err.message })
+  }
+}
+
 function Callback (res) {
   return function (err, result) {
     if (err) console.log(err)
@@ -140,6 +177,18 @@ function Callback (res) {
       ? res.status(422).json({ error: err.message })
       : res.status(200).json(result)
   }
+}
+
+async function processIterator (iterator, res, maxEntries) {
+  if (typeof maxEntries === 'string') maxEntries = parseInt(maxEntries)
+  maxEntries = maxEntries || defaultMaxEntries
+  const output = []
+  for await (const entry of iterator) {
+    output.push(entry)
+    if (output.length >= maxEntries) break
+  }
+  // TODO handle errors
+  return res.status(200).json(output)
 }
 
 function pullback (stream, res, maxEntries) {
@@ -151,10 +200,10 @@ function pullback (stream, res, maxEntries) {
   )
 }
 
-function build (uiFilePath, options, callback) {
+async function build (uiFilePath, options) {
   const uiFile = fs.createWriteStream(uiFilePath)
-  uiFile.on('error', (err) => { return callback(err) })
+  uiFile.on('error', (err) => { return Promise.reject(err) })
   const ui = buildUi(options)
-  ui.on('end', () => { callback() })
+  // ui.on('end', () => { callback() })
   ui.pipe(uiFile)
 }
